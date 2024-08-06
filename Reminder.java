d5f61e820a3a5a061a1dbc9f4b5072788f7380c0
Eracks1012 <110631885+Eracks1012@users.noreply.github.com>
2024-08-06 15:06:37 -0400

    Create Reminder.java

---
+++ Reminder.java
@@ -0,0 +1,25 @@
+public class Reminder {
+    private int reminderID;
+    private int applicationID;
+    private String reminderDate;
+    private String reminderMessage;
+
+    // Constructor
+    public Reminder(int reminderID, int applicationID, String reminderDate, String reminderMessage) {
+        this.reminderID = reminderID;
+        this.applicationID = applicationID;
+        this.reminderDate = reminderDate;
+        this.reminderMessage = reminderMessage;
+    }
+
+    // Methods
+    public void setReminder() {
+    }
+
+    public void getReminders() {
+    }
+
+    public void deleteReminder() {
+    }
+
+}
