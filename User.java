ac6962b79f964dff130f14df8589b4a4c52fe4eb
Eracks1012 <110631885+Eracks1012@users.noreply.github.com>
2024-08-06 14:51:58 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,35 @@
+public class User {
+    private int userID;
+    private String username;
+    private String password;
+    private String email;
+    private String profileDetails;
+    private List<JobApplication> dashboard;
 
+    // Constructor
+    public User(int userID, String username, String password, String email, String profileDetails) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.profileDetails = profileDetails;
+        this.dashboard = new ArrayList<>();
+    }
+
+    // Methods
+    public void register() {
+        // Implementation here
+    }
+
+    public void login() {
+        // Implementation here
+    }
+
+    public void updateProfile(String newDetails) {
+        // Implementation here
+    }
+
+    public List<JobApplication> viewDashboard() {
+        return dashboard;
+    }
+}
