a29c28523da1fce5e5b83b53145692674adf80a2
yz6973 <157756166+yz6973@users.noreply.github.com>
2024-08-08 19:26:16 -0400

    Update Reminder.java

--- Reminder.java
+++ Reminder.java
@@ -13,13 +13,46 @@ public class Reminder {
     }
 
     // Methods
-    public void setReminder() {
+    public void setReminder(Date newReminderDate, String newReminderMessage) {
+        this.reminderDate = newReminderDate;
+        this.reminderMessage = newReminderMessage;
+        System.out.println("Reminder set for application ID: " + applicationID + " on " + reminderDate);
     }
 
-    public void getReminders() {
+    public String getReminderDetails() {
+        return "Reminder ID: " + reminderID + ", Application ID: " + applicationID +
+               ", Date: " + reminderDate + ", Message: " + reminderMessage;
     }
 
     public void deleteReminder() {
+        // Logic to delete the reminder from the system
+        System.out.println("Reminder deleted for application ID: " + applicationID);
+    }
+
+    // Getters for the private fields
+    public int getReminderID() {
+        return reminderID;
+    }
+
+    public int getApplicationID() {
+        return applicationID;
+    }
+
+    public Date getReminderDate() {
+        return reminderDate;
+    }
+
+    public String getReminderMessage() {
+        return reminderMessage;
+    }
+
+    // Setters for the private fields
+    public void setReminderDate(Date reminderDate) {
+        this.reminderDate = reminderDate;
+    }
+
+    public void setReminderMessage(String reminderMessage) {
+        this.reminderMessage = reminderMessage;
     }
 
 }
